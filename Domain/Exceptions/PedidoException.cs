0b657739fe817098cef533301e7718bd2f74207c
abmed <ab.medina.011@gmail.com>
2026-07-24 20:12:55 -0500

    feat: Añadir Value Object y dos exceptions personalizadas

---
+++ Domain/Exceptions/PedidoException.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Exceptions
+{
+    public class PedidoException : Exception
+    {
+        public PedidoException(string mensaje) : base(mensaje) { }
+    }
+
+    public class StockInsuficienteException : Exception
+    {
+        public Guid ProductoId { get; }
+        public int CantidadDisponible { get; }
+        public int CantidadSolicitada { get; }
+        public StockInsuficienteException(
+            Guid productoId,
+            int cantidadDisponible,
+            int cantidadSolicitada)
+            :base($"Stock insuficiente para el producto {productoId}. " + $"Disponible: {cantidadDisponible}, Solicitado: {cantidadSolicitada}")
+        {
+            ProductoId = productoId;
+            CantidadDisponible = cantidadDisponible;
+            CantidadSolicitada = cantidadSolicitada;
+        }
+    }
+}
---
+++ Domain/ValueObjects/Dinero.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.ValueObjects
+{
+    public record Dinero
+    {
+        public decimal Monto { get; }
+
+        public Dinero(decimal monto)
+        {
+            if (monto < 0) throw new ArgumentException("El monto no puede ser negativo.");
+            Monto = Math.Round(monto, 2);
+        }
+
+        public static Dinero Cero => new(0);
+
+        public Dinero Sumar(Dinero otro) => new(Monto + otro.Monto);
+        public Dinero Multiplicar(int cantidad) => new(Monto * cantidad);
+
+        public static implicit operator decimal(Dinero dinero) => dinero.Monto;
+
+        public override string ToString() => $"S/ {Monto:F2}";
+    }
+}
